3301ae9614c463834956d28d68e97a40b0aa09d1
egusheva <emaguseva@gmail.com>
2023-04-13 10:06:03 +0200

    Add files via upload

---
+++ Sectorial coding tree.R
@@ -0,0 +1,5 @@
+library(collapsibleTree)
+Sectors <- read.csv("/Users/egusheva/Library/Mobile Documents/com~apple~CloudDocs/Documents/Work/TU Delft/Publications/SPMs review/Graphs/Data for sectorial coding tree.csv", header = TRUE, sep=";")
+head(Sectors)
+p <- collapsibleTree(Sectors, c("Level1", "Level2"))
+p
---
+++ sectorial-sunburst.R
@@ -0,0 +1,21 @@
+# libraries
+library(tidyverse)
+library(treemap)
+library(sunburstR)
+
+# Load dataset
+data <- read.csv("/Users/egusheva/Library/Mobile Documents/com~apple~CloudDocs/Documents/Work/TU Delft/Publications/SPMs review/Graphs/Sunburst plot/Data for AR5 sectors.csv", header=TRUE, sep=";")
+data[ which(data$value==-1),"Absolutecount"] <- 1
+colnames(data) <- c("Level1", "Absolutecount")
+data
+
+# Reformat data for the sunburstR package
+data <- data %>%
+  filter(Level1 != "") %>%
+  mutate(path = paste(Level1, sep="-")) %>%
+  dplyr::select(path, Absolutecount)
+data
+
+# Plot
+p <- sunburst(data, legend=FALSE)
+p
