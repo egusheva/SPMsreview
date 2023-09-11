13901104caa89a6edfc1396e6b9d9f2eba70fee7
egusheva <emaguseva@gmail.com>
2023-09-11 10:11:02 +0200

    Add files via upload

--- Thematic coding tree.R
+++ Thematic coding tree.R
@@ -1,5 +1,5 @@
 library(collapsibleTree)
-Themes <- read.csv("/Users/egusheva/Library/Mobile Documents/com~apple~CloudDocs/Documents/Work/TU Delft/Publications/SPMs review/Graphs/Data for thematic coding tree.csv", header = TRUE, sep=";")
+Themes <- read.csv("/Users/egusheva/Library/Mobile Documents/com~apple~CloudDocs/Documents/Work/TU Delft/Publications/SPMs review/Method/Trends analysis and comparison/Graphs/Coding tree/Data for thematic coding tree.csv", header = TRUE, sep=";")
 head(Themes)
 p <- collapsibleTree(Themes, c("Level1", "Level2", "Level3", "Level4"))
 p
